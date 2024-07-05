529d2fe97def69abcfc908e3b2d8193df3637022
vSahib <tradersahxb@gmail.com>
2024-07-05 04:02:16 -0700

    CSS Box Model

---
+++ script.py
@@ -0,0 +1,17 @@
+# print("hello world")
+
+# for i in range(0,20):
+#     print(i)
+
+# for index, number in enumerate(range(1,21+1)):
+#     if number%2 !=0: 
+#         print(f"{index}:{number}")
+    
+
+# for i in range(0,100):
+#     print(i)
+
+for j in range(0,1000000):
+    print(j)
+    for q in range(0,1000000):
+        print(q)
